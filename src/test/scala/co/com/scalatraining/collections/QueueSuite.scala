b45d0a8e7380fabc1d9a0b51cad9a4650f803c02
S4N <santi_rm8@hotmail.com>
2018-07-24 14:44:52 -0500

    Adición de las suites Queue y Stack con ejemplos

--- src/test/scala/co/com/scalatraining/collections/QueueSuite.scala
+++ src/test/scala/co/com/scalatraining/collections/QueueSuite.scala
@@ -1,5 +1,113 @@
 package co.com.scalatraining.collections
+import scala.collection.mutable.Queue
 
-class QueueSuite {
+import org.scalatest.FunSuite
 
+class QueueSuite extends FunSuite{
+
+  test("Creacion vacia") {
+    val cola = Queue[Int]()
+    println(cola)
+    assert(cola.isEmpty)
+  }
+
+  test("Creacion de cola") {
+    val cola = Queue[Int](1,2,3,4)
+    println(cola)
+    assert(cola === Queue(1,2,3,4))
+  }
+
+  test("Agregar elementos a la cola"){
+    val cola = Queue(1,2,3)
+    assertResult(Queue(1,2,3,4)) {
+      cola += 4
+    }
+  }
+
+  /*test("Agregar elementos a la cola con enqueue"){
+    var cola = Queue[String]("1","2","3")
+    assertResult(Queue("1","2","3","4")) {
+      cola.enqueue("4")
+    }
+  }*/
+
+  test("Agregar elementos a la cola desde una lista"){
+    val cola = Queue("Santiago", "Camilo", "Juan")
+    assertResult(Queue("Santiago", "Camilo", "Juan", "Mauricio", "Miguel")) {
+      cola ++= List("Mauricio", "Miguel")
+    }
+  }
+
+  test("Eliminar de la cola el primero"){
+    val cola = Queue("Santiago", "Camilo", "Juan")
+
+    cola.dequeue()
+
+    assert(Queue("Camilo", "Juan") === cola)
+  }
+
+  test("Eliminar de la cola el primero que cumpla con la condición"){
+    val cola = Queue("Santiago", "Camilo", "Juan")
+
+    cola.dequeueFirst(_.startsWith("C"))
+
+    assert(Queue("Santiago", "Juan") === cola)
+  }
+
+  test("Eliminar de la cola todos los que cumplan con la condición"){
+    val cola = Queue("Santiago", "Camilo", "Juan", "Cristina")
+
+    cola.dequeueAll(_.startsWith("C"))
+
+    assert(Queue("Santiago", "Juan") === cola)
+  }
+
+  test("Recorrer una cola con foreach"){
+    val cola = Queue(1, 2, 3, 4)
+    var sum = 0
+    assertResult(10){
+      cola.foreach((x) =>
+        sum += x
+      )
+      sum
+    }
+  }
+
+  test("Head en un Queue"){
+    val cola = Queue(1,2,3)
+    assertResult(1){
+      cola.head
+    }
+  }
+
+  test("tail en un Queue"){
+    val cola = Queue(1,2,3)
+    assertResult(Queue(2,3)){
+      cola.tail
+    }
+  }
+
+  test("split en un Queue"){
+    val cola = Queue(1,2,3)
+    val (cola2, cola3) = cola.splitAt(2)
+    assert(cola2 === Queue(1,2) && cola3 === Queue(3))
+  }
+
+  test("filter en un Queue"){
+    val cola = Queue(1,2,3,4,5,6,7,8,9,10)
+    assertResult(Queue(2,4,6,8,10)){
+      cola.filter(dato =>
+        dato % 2 == 0
+      )
+    }
+  }
+
+  test("Transformación de un Queue con map"){
+    val cola = Queue(1,2,3,4,5)
+    assertResult(Queue(2,4,6,8,10)){
+      cola.map(dato =>
+        dato * 2
+      )
+    }
+  }
 }
--- src/test/scala/co/com/scalatraining/collections/StackSuite.scala
+++ src/test/scala/co/com/scalatraining/collections/StackSuite.scala
@@ -1,5 +1,95 @@
 package co.com.scalatraining.collections
 
-class StackSuite {
+import org.scalatest.FunSuite
 
+import scala.collection.mutable.Stack
+
+class StackSuite extends FunSuite {
+
+  test("Creacion vacia") {
+    val pila = Stack[Int]()
+    println(pila)
+    assert(pila.isEmpty)
+  }
+
+  test("Creacion de pila") {
+    val pila = Stack[Int](1,2,3,4)
+    println(pila)
+    assert(pila === Stack(1,2,3,4))
+  }
+
+  test("Agregar elementos a la pila"){
+    val pila = Stack(1,2,3)
+    assertResult(Stack(4,1,2,3)) {
+      pila.push(4)
+    }
+  }
+
+  test("Eliminar elemento de la pila"){
+    val pila = Stack(1,2,3)
+    pila.pop()
+    assert(pila === Stack(2,3))
+  }
+
+  test("Conocer el primero en la pila"){
+    val pila = Stack(1,2,3)
+    assertResult(1){
+      pila.top
+    }
+  }
+
+  test("Conocer el tamaño de la pila"){
+    val pila = Stack(1,2,3)
+    assertResult(3){
+      pila.size
+    }
+  }
+
+  test("Limpiar la pila"){
+    val pila = Stack(1,2,3)
+    pila.clear()
+    assert(pila.isEmpty)
+  }
+
+  test("tail en un Stack"){
+    val cola = Stack(1,2,3)
+    assertResult(Stack(2,3)){
+      cola.tail
+    }
+  }
+
+  test("Recorrer una pila con foreach"){
+    val pila = Stack(1, 2, 3, 4, 5)
+    var sum = 0
+    assertResult(15){
+      pila.foreach((x) =>
+        sum += x
+      )
+      sum
+    }
+  }
+
+  test("split en una pila"){
+    val pila = Stack(1,2,3)
+    val (pila2, pila3) = pila.splitAt(2)
+    assert(pila2 === Stack(1,2) && pila3 === Stack(3))
+  }
+
+  test("filter en una pila"){
+    val pila = Stack(1,2,3,4,5,6,7,8,9,10)
+    assertResult(Stack(2,4,6,8,10)){
+      pila.filter(dato =>
+        dato % 2 == 0
+      )
+    }
+  }
+
+  test("Transformación de una pila con map"){
+    val pila = Stack(1,2,3,4,5)
+    assertResult(Stack(2,4,6,8,10)){
+      pila.map(dato =>
+        dato * 2
+      )
+    }
+  }
 }
